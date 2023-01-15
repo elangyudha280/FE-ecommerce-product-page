efc96677ca5a8e762b3ec90c236bf18ee9b8657b
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-01-15 10:54:29 +0700

    setup react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <main className="App">
+  
+    </main>
+  )
+}
+
+export default App
