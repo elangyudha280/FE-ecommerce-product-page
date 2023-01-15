d0797a55de223d7b22785b8b8b1495aaa3ad5194
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-01-15 11:25:52 +0700

    setup custom value class utility tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 
 function App() {
 
   return (
-    <main className="App">
+    <main className="App relative">
   
     </main>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'Kumbh-Sans': ["'Kumbh Sans'"],
+      },
+      colors:{
+        'ecommerce-orange':"hsl(26, 100%, 55%)",
+        'ecommerce-pale-orange':'hsl(25, 100%, 94%)',
+        'ecommerce-very-dark-blue':'hsl(220, 13%, 13%)',
+        'ecommerce-dark-grayish-blue':'hsl(219, 9%, 45%)',
+        'ecommerce-grayish-blue':'hsl(220, 14%, 75%)',
+        'ecommerce-light-grayish-blue':'hsl(223, 64%, 98%)',
+        
+      }
+    },
   },
   plugins: [],
 }
