e46f9c6ed119075dba2bb028163eb6941f64788f
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-01-17 20:01:11 +0700

    mulai desain component container description sneaker

--- src/component/ContainerSneakers.jsx
+++ src/component/ContainerSneakers.jsx
@@ -12,17 +12,17 @@ const ContainerSneakers =()=>{
                         <img src="./images/image-product-1.jpg" alt="" className="img-content-thumb object-cover object-top w-full h-full absolute select-none" />
                     </div>
                     <div className="item-tumb w-full pt-2 px-2  flex items-center justify-between">
-                            <div className="thumb-item w-[70px] rounded-lg cursor-pointer overflow-hidden h-[70px] ">
-                            <img src="./images/image-product-1.jpg" alt="" className="img-item-thumb object-cover object-top w-full h-full  select-none" />
+                            <div className="thumb-item group w-[70px]  rounded-lg cursor-pointer overflow-hidden h-[70px] hover:shadow-[0_0_0_2px_#ff7d1a] ">
+                            <img src="./images/image-product-1.jpg" alt="" className="img-item-thumb transition-all duration-200 group-hover:opacity-40 object-cover object-top w-full h-full  select-none" />
                             </div>
-                            <div className="thumb-item w-[70px] rounded-lg cursor-pointer overflow-hidden h-[70px] ">
-                            <img src="./images/image-product-2.jpg" alt="" className="img-item-thumb object-cover object-top w-full h-full  select-none" />
+                            <div className="thumb-item  group w-[70px] rounded-lg cursor-pointer overflow-hidden h-[70px] hover:shadow-[0_0_0_2px_#ff7d1a] ">
+                            <img src="./images/image-product-2.jpg" alt="" className="img-item-thumb transition-all duration-200 group-hover:opacity-40 object-cover object-top w-full h-full  select-none" />
                             </div>
-                            <div className="thumb-item w-[70px] rounded-lg cursor-pointer overflow-hidden h-[70px] ">
-                            <img src="./images/image-product-3.jpg" alt="" className="img-item-thumb object-cover object-top w-full h-full  select-none" />
+                            <div className="thumb-item group w-[70px] rounded-lg cursor-pointer overflow-hidden h-[70px] hover:shadow-[0_0_0_2px_#ff7d1a] ">
+                            <img src="./images/image-product-3.jpg" alt="" className="img-item-thumb transition-all duration-200 group-hover:opacity-40 object-cover object-top w-full h-full  select-none" />
                             </div>
-                            <div className="thumb-item w-[70px] rounded-lg cursor-pointer overflow-hidden h-[70px] ">
-                            <img src="./images/image-product-4.jpg" alt="" className="img-item-thumb object-cover object-top w-full h-full  select-none" />
+                            <div className="thumb-item group w-[70px] rounded-lg cursor-pointer overflow-hidden h-[70px] hover:shadow-[0_0_0_2px_#ff7d1a] ">
+                            <img src="./images/image-product-4.jpg" alt="" className="img-item-thumb transition-all duration-200 group-hover:opacity-40 object-cover object-top w-full h-full  select-none" />
                             </div>
                                
                     </div>
