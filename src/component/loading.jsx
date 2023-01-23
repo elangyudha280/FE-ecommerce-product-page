b65ddd05da6188fadb13e241c03bd6041621d185
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-01-23 14:52:40 +0700

    mulai fitur loading

---
+++ src/component/loading.jsx
@@ -0,0 +1,17 @@
+
+
+const Loading = ()=>{
+    return (
+        <main className="loading transition-all duration-300 relative w-full h-[100vh] grid place-items-center">
+            <div className="meetup">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </main>
+    )
+}
+
+
+export default Loading;
